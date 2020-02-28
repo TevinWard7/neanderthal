83521bc2a25442a0205564fe0ad01c5355e6b453
Tevin <tevinward7@gmail.com>
2020-02-27 23:49:56 -0500

    added javascript variables and alerts

---
+++ script.js
@@ -0,0 +1,9 @@
+var areYouAHuman = confirm ("Are you a human?")
+
+if (areYouAHuman) {
+    alert ("Congrats Sapien, enjoy site!")
+}
+
+else {
+    alert ("You're not telling the truth, caught ya!")
+}
