8cf3d6576e6c1071f4b2179700abe0cc2a64e029
Tevin <tevinward7@gmail.com>
2020-02-28 01:50:45 -0500

    added more javascript and some comments

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-var areYouAHuman = confirm ("Are you a human?")
+
+//Question 1//
+var areYouAHuman = confirm ("Are you a human?");
 
 if (areYouAHuman) {
-    alert ("Congrats Sapien, enjoy site!")
+    alert ("Congrats!");
 }
 
 else {
-    alert ("You're not telling the truth, caught ya!")
+    alert ("You're not telling the truth, caught ya!");
 }
+
+//Question 2//
+var kindOfHuman = ["homo sapien", "sapien"];
+
+var userHumanGuess = prompt ("What kind of human!");
+
+if (kindOfHuman.indexOf(userHumanGuess) == -1 ) {
+    alert ("Hmmmm");
+}
+
+else {
+    alert ("Enoy the site Sapien!")
+}
+
